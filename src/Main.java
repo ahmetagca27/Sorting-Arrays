06633d84bfcaa212811a5effab8f78fd40b00521
Ahmet AGCA <113193339+ahmetagca27@users.noreply.github.com>
2023-06-19 15:26:36 +0300

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,18 +6,19 @@ public class Main {
        Scanner input = new Scanner(System.in);
 
         //We ask the user to enter the Arrays length
-       System.out.println("Dizi Uzunluğunu Giriniz \t.:");
+       System.out.print("Dizi Uzunluğunu Giriniz \t.:");
         int b = input.nextInt();
         int [] arrays = new int[b];
 
         //Adding the numbers entered by the user to the array
         for(int i=0; i<arrays.length; i++){
-            System.out.print(i+1 + ". sayıyı giriniz \t.:");
+            System.out.print(i+1 + ". sayıyı giriniz \t\t\t.:");
             arrays[i] = input.nextInt();
         }
 
         //We sort our array from smallest to largest
         Arrays.sort(arrays);
+        System.out.println("------------------------------");
         System.out.print("Küçükten Büyüğe Sıralama \t .:" + Arrays.toString(arrays));
 
     }
